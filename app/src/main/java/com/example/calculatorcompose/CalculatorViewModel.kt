bf16d96c5a87845e6f2bced04db9c77cb249e2ab
Abinash Acharya <075bei004.abinash@pcampus.edu.np>
2023-08-17 00:13:22 +0545

    Remove Access AC Buttons.

--- app/src/main/java/com/example/calculatorcompose/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculatorcompose/CalculatorViewModel.kt
@@ -39,62 +39,65 @@ class CalculatorViewModel: ViewModel() {
         val number1 = state.number1.toDoubleOrNull()
         val number2 = state.number2.toDoubleOrNull()
 
-        if(number1!= null && number2!= null)
-            val result = when(state.operation){
+        if (number1 != null && number2 != null) {
+            val result = when (state.operation) {
                 is CalculatorOperation.Add -> number1 + number2
                 is CalculatorOperation.Subtract -> number1 - number2
                 is CalculatorOperation.Muntiply -> number1 * number2
                 is CalculatorOperation.Divide -> number1 / number2
-            null -> return
-        }
-        state = state.copy(
-            number1 = result.toString().take(15),
-            number2="",
-            operation = null
-        )
-
-    }
-
-    private fun enterOperation(operation: CalculatorOperation) {
-        if(state.number1.isNotBlank()){
-            state = state.copy(operation = operation)
-        }
-    }
-
-    private fun enterDecimal() {
-        if (state.operation == null && !state.number1.contains(".")
-            && state.number1.isNotBlank()
-        ){
+                null -> return
+            }
             state = state.copy(
-                    number1 = state.number1 + "."
-                    )
-            return
-        }
-        if (state.number2.contains(".") && !state.number2.isNotBlank()
-            && state.number1.isNotBlank()
-        ){
-            state = state.copy(
-                number1 = state.number2 + "."
+                number1 = result.toString().take(15),
+                number2 = "",
+                operation = null
             )
-            return
         }
     }
 
-    private fun enterNumber(number: Int) {
-        if(state.operation == null){
-            if(state.number1.length >= MAX_NUM_LENGTH){
+        private fun enterOperation(operation: CalculatorOperation) {
+            if (state.number1.isNotBlank()) {
+                state = state.copy(operation = operation)
+            }
+        }
+
+        private fun enterDecimal() {
+            if (state.operation == null && !state.number1.contains(".")
+                && state.number1.isNotBlank()
+            ) {
+                state = state.copy(
+                    number1 = state.number1 + "."
+                )
+                return
+            }
+            if (state.number2.contains(".") && !state.number2.isNotBlank()
+                && state.number1.isNotBlank()
+            ) {
+                state = state.copy(
+                    number1 = state.number2 + "."
+                )
+                return
+            }
+        }
+
+        private fun enterNumber(number: Int) {
+            if (state.operation == null) {
+                if (state.number1.length >= MAX_NUM_LENGTH) {
+                    return
+                }
+                state = state.copy(
+                    number1 = state.number1 + number
+                )
+                return
+            }
+            if (state.number2.length >= MAX_NUM_LENGTH) {
                 return
             }
             state = state.copy(
-                number1 =state.number1 + number
+                number2 = state.number2 + number
             )
-            return
         }
-        if(state.number2.length>=MAX_NUM_LENGTH){
-            return
-        }
-        state =state.copy((
-                number2 =   state.number2 + number
-                ))
+    companion object{
+        private const val MAX_NUM_LENGTH = 15
     }
 }
